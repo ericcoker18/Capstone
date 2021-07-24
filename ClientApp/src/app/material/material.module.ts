21dec1cfd89b2cec2d8c5cbfa11846ef340f5f50
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-07-24 13:37:01 -0500

    column and material

--- ClientApp/src/app/material/material.module.ts
+++ ClientApp/src/app/material/material.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {MatSelectModule} from "@angular/material/select";
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSelectModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
-  exports: [MatSelectModule]
+  exports: [
+    MatSelectModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
+  ]
 })
-export class MaterialModule { }
+export class MaterialModule {
+}
